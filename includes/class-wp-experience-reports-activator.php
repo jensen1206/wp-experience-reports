abc48343bd7afede051565fb74b56cd2d2da5bc4
jensen1206 <jens@harz-web.com>
2022-03-11 09:53:14 +0100

    first commit

---
+++ includes/class-wp-experience-reports-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wwdh.de
+ * @since      1.0.0
+ *
+ * @package    Wp_Experience_Reports
+ * @subpackage Wp_Experience_Reports/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Experience_Reports
+ * @subpackage Wp_Experience_Reports/includes
+ * @author     Jens Wiecker <email@jenswiecker.de>
+ */
+class Wp_Experience_Reports_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ wp-experience-reports.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              https://wwdh.de
+ * @since             1.0.0
+ * @package           Wp_Experience_Reports
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Experience Reports
+ * Plugin URI:        https://wwdh.de/plugins
+ * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
+ * Version:           1.0.0
+ * Author:            Jens Wiecker
+ * Author URI:        https://wwdh.de
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-experience-reports
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define( 'WP_EXPERIENCE_REPORTS_VERSION', '1.0.0' );
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-wp-experience-reports-activator.php
+ */
+function activate_wp_experience_reports() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-experience-reports-activator.php';
+	Wp_Experience_Reports_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-wp-experience-reports-deactivator.php
+ */
+function deactivate_wp_experience_reports() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-experience-reports-deactivator.php';
+	Wp_Experience_Reports_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_wp_experience_reports' );
+register_deactivation_hook( __FILE__, 'deactivate_wp_experience_reports' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-wp-experience-reports.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_wp_experience_reports() {
+
+	$plugin = new Wp_Experience_Reports();
+	$plugin->run();
+
+}
+run_wp_experience_reports();
